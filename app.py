6436c21c0c3bb3a2396c1242457e9dcd28d03a2b
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-16 21:37:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-#from qfin.simulations import GeometricBrownianMotion
+from qfin.simulations import GeometricBrownianMotion
+#from "https://github.com/RomanMichaelPaolucci/Q-Fin" import GeometricBrownianMotion
 
 st.write("My First Streamlit Web App")
 
