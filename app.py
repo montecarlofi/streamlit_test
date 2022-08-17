7a77a1ced604b1c7e5654aea961396fcec1be9d4
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-17 18:30:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from random import random
 #from qfin.simulations import GeometricBrownianMotion
 #from "https://github.com/RomanMichaelPaolucci/Q-Fin" import GeometricBrownianMotion
 
