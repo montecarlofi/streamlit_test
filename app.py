f24dfa616a384716d6ead2be22400b17f782b175
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-17 11:00:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 import pandas as pd
 import streamlit as st
-from qfin.simulations import GeometricBrownianMotion
+#from qfin.simulations import GeometricBrownianMotion
 #from "https://github.com/RomanMichaelPaolucci/Q-Fin" import GeometricBrownianMotion
 
 st.write("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
+
+with st.echo(code_location='below'):
+  chart_data = pd.DataFrame()
+  
+  S = st.slider('Initial Stock Price: ', 0, 100, 50, 5)
+  mu = st.slider('Drift %: ', 0, 30, 0, 1)
+  sigma = st.slider('Volatility %: ', 0, 30, 15, 1)
+  T = st.slider('T (mos): ', 0, 100, 50, 10)
+  n = st.slider('Simulations: ', 0, 100, 50, 10) 
+  
+  # 100 - initial underlying asset price
+  # 0 - underlying asset drift (mu)
+  # .3 - underlying asset volatility
+  # 1/52 - time steps (dt)
+  # 1 - time to maturity (annum)
+  # gbm = GeometricBrownianMotion(100, 0, .3, 1/52, 1)
+ 
+  for i in range(n):
+    chart_data[str(i)] = GeometricBrownianMotion(S, mu/100, sigma/100, 1/252, T/12).s
+    
+  st.line_chart(chart_data)
