70e9e0601406e757790c735f90ef96b084a2fd25
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-18 21:49:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ import math
 
 st.write("Monte Carlo . fi")
 
-with st.echo(code_location='below'):
-  chart_data = pd.DataFrame()
-  
-  S = st.slider('Initial Stock Price: ', 0, 100, 50, 5)
-  mu = st.slider('Drift %: ', 0, 30, 0, 1)
-  sigma = st.slider('Volatility %: ', 0, 30, 15, 1)
-  T = st.slider('T (mos): ', 0, 100, 50, 10)
-  n = st.slider('Simulations: ', 0, 100, 50, 10) 
+chart_data = pd.DataFrame()
+
+S = st.slider('Initial Stock Price: ', 0, 100, 50, 5)
+mu = st.slider('Drift %: ', 0, 30, 0, 1)
+sigma = st.slider('Volatility %: ', 0, 30, 15, 1)
+T = st.slider('T (mos): ', 0, 100, 50, 10)
+n = st.slider('Simulations: ', 0, 100, 50, 10) 
 
 
 def get_historical_data(market):
