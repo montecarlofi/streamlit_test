e659a0d83f48658d02c50fc2c30902f527c66f00
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-18 21:56:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,6 @@ x_index.extend(range(0,len(paths_combined)))
 #axall.plot(x_index, paths_combined, 'r')
 
 
-chart_data[str(i)] = chart[0]
+chart_data[str(i)] = chart
 
 st.line_chart(chart_data)
