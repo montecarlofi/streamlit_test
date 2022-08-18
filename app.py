d1db6ec110526067601a1d8d7a00160a44b5fff7
montecarlofi <111383749+montecarlofi@users.noreply.github.com>
2022-08-18 22:22:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ import math
 
 st.write("Monte Carlo . fi")
 
-with st.echo(code_location='below'):
-    chart_data = pd.DataFrame()
-
-    S = st.slider('Initial Stock Price: ', 0, 100, 50, 5)
-    mu = st.slider('Drift %: ', 0, 30, 0, 1)
-    volatility = st.slider('Volatility %: ', 0, 30, 15, 1)
-    st.write("Volatility = ", volatility)
-    T = st.slider('T (mos): ', 0, 100, 50, 10)
-    n = st.slider('Simulations: ', 0, 100, 50, 10) 
-
 def get_historical_data(market):
     # market choice and where and how to pull data from it is not yet implemented
     # so making it randomly for now
@@ -65,66 +55,77 @@ def geometric_series(rates_of_movements, start_value):
     return series
 
 
-### User variables ###
-#volatility = 0.3
-rate = 1.07
-periods_per_rate = 252 # E.g., trading days per yearly rate.
-size_of_each_resample = 252 # How many days do I want for my sample?
-years = 4
+with st.echo(code_location='below'):
+    chart_data = pd.DataFrame()
+
+    S = st.slider('Initial Stock Price: ', 0, 100, 50, 5)
+    mu = st.slider('Drift %: ', 0, 30, 0, 1)
+    volatility = st.slider('Volatility %: ', 0, 30, 15, 1)
+    st.write("Volatility = ", volatility)
+    T = st.slider('T (mos): ', 0, 100, 50, 10)
+    n = st.slider('Simulations: ', 0, 100, 50, 10) 
 
 
-### Script ###
-size_of_history = 252*50
-history, largest, smallest = get_random_series(rate, volatility, size_of_history, periods_per_rate)
-#history = get_historical_data("Nor_Stock_Market") # market choice not yet implemented
-#print("History ", history, "\n")
-#print("Largest: ", largest, "\nSmallest: ", smallest, "\n")
+    ### User variables ###
+    #volatility = 0.3
+    rate = 1.07
+    periods_per_rate = 252 # E.g., trading days per yearly rate.
+    size_of_each_resample = 252 # How many days do I want for my sample?
+    years = 4
 
-chart = []
-start_value = 1
-resampled = resample(history, size_of_each_resample)
-#print("Resampled ", resampled)
-geo = geometric_series(resampled, start_value)
-chart.append(geo)
-highest_index = len(geo) - 1
-last_value = geo[highest_index]
-for i in range(1, years):
+
+    ### Script ###
+    size_of_history = 252*50
+    history, largest, smallest = get_random_series(rate, volatility, size_of_history, periods_per_rate)
+    #history = get_historical_data("Nor_Stock_Market") # market choice not yet implemented
+    #print("History ", history, "\n")
+    #print("Largest: ", largest, "\nSmallest: ", smallest, "\n")
+
+    chart = []
+    start_value = 1
     resampled = resample(history, size_of_each_resample)
-    row = geometric_series(resampled, last_value)
+    #print("Resampled ", resampled)
+    geo = geometric_series(resampled, start_value)
+    chart.append(geo)
     highest_index = len(geo) - 1
-    last_value = row[highest_index]
-    #chart[i] = geo
-    chart.append(row)
+    last_value = geo[highest_index]
+    for i in range(1, years):
+        resampled = resample(history, size_of_each_resample)
+        row = geometric_series(resampled, last_value)
+        highest_index = len(geo) - 1
+        last_value = row[highest_index]
+        #chart[i] = geo
+        chart.append(row)
 
-#print("Chart \n ", chart[1])
+    #print("Chart \n ", chart[1])
 
-#figure, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, sharex=True)
-#figure, (ax1, axall, axRndDist) = plt.subplots(3, constrained_layout = True)
-#plt.xticks(np.arange(0, len(x), 10))
+    #figure, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, sharex=True)
+    #figure, (ax1, axall, axRndDist) = plt.subplots(3, constrained_layout = True)
+    #plt.xticks(np.arange(0, len(x), 10))
 
-#ax1.set_title("Year 1")
-x1_index = []
-x1_index.extend(range(0,len(chart[0])))
-for i in range(0, len(chart)):
-    #ax1.plot(x1_index, chart[i])
-    pass
+    #ax1.set_title("Year 1")
+    x1_index = []
+    x1_index.extend(range(0,len(chart[0])))
+    for i in range(0, len(chart)):
+        #ax1.plot(x1_index, chart[i])
+        pass
 
-#axall.set_title("All {} years".format(years))
-paths_combined = []
-for i in range(0, len(chart)):
-    temp_series = chart[i]
-    for k in range(0, len(temp_series)):
-        paths_combined.append(temp_series[int(k)])
-# Works:
-#paths_combined = []
-#for i in range(0, len(chart)):
-#    paths_combined.extend(chart[int(i)])
-x_index = []
-x_index.extend(range(0,len(paths_combined)))
-#axall.plot(x_index, paths_combined, 'r')
+    #axall.set_title("All {} years".format(years))
+    paths_combined = []
+    for i in range(0, len(chart)):
+        temp_series = chart[i]
+        for k in range(0, len(temp_series)):
+            paths_combined.append(temp_series[int(k)])
+    # Works:
+    #paths_combined = []
+    #for i in range(0, len(chart)):
+    #    paths_combined.extend(chart[int(i)])
+    x_index = []
+    x_index.extend(range(0,len(paths_combined)))
+    #axall.plot(x_index, paths_combined, 'r')
 
-#chart_data = []
-for i in range(0, len(chart)):
-#    chart_data.extend(chart[int(i)])
-    chart_data[str(i)] = chart[int(i)]
-st.line_chart(chart_data)
+    #chart_data = []
+    for i in range(0, len(chart)):
+    #    chart_data.extend(chart[int(i)])
+        chart_data[str(i)] = chart[int(i)]
+    st.line_chart(chart_data)
